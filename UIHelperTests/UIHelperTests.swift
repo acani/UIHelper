57ec900c51147c29d4ba51b84986145dec1d079a
Matthew Di Pasquale <liveloveprosper@gmail.com>
2016-06-13 09:51:47 -0400

    1st commit: Swift Cocoa Touch Framework with tests

---
+++ UIHelperTests/UIHelperTests.swift
@@ -0,0 +1,36 @@
+//
+//  UIHelperTests.swift
+//  UIHelperTests
+//
+//  Created by Matthew Di Pasquale on 6/13/16.
+//  Copyright © 2016 Acani LLC. All rights reserved.
+//
+
+import XCTest
+@testable import UIHelper
+
+class UIHelperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
