1e8bc931bc74fae2d86fe54c46ffa0fddc8ddc03
Matthew Di Pasquale <liveloveprosper@gmail.com>
2016-11-20 01:37:19 -0500

    Confirm !topmostViewController.isBeingDismissed

--- UIHelper/UIHelper.swift
+++ UIHelper/UIHelper.swift
@@ -3,7 +3,7 @@ import UIKit
 extension UIApplication {
     public static var auh_topmostViewController: UIViewController {
         var topmostViewController = UIApplication.shared.delegate!.window!!.rootViewController!
-        while let presentedViewController = topmostViewController.presentedViewController {
+        while let presentedViewController = topmostViewController.presentedViewController, !presentedViewController.isBeingDismissed {
             topmostViewController = presentedViewController
         }
         return topmostViewController
