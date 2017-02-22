b20c153f4b6eb1ce3e9e4e70c4b0f54f337d1e5e
Matthew Di Pasquale <liveloveprosper@gmail.com>
2017-02-21 20:18:02 -0500

    Remove UITableViewCell.auh_isEnabled
    Co-Authored-By: Matt Packer <matthewrpacker@gmail.com>

--- UIHelper/UIHelper.swift
+++ UIHelper/UIHelper.swift
@@ -10,17 +10,6 @@ extension UIApplication {
     }
 }
 
-extension UITableViewCell {
-    open var auh_isEnabled: Bool {
-        get { return isUserInteractionEnabled }
-        set {
-            isUserInteractionEnabled = newValue
-            textLabel!.isEnabled = newValue
-            detailTextLabel!.isEnabled = newValue
-        }
-    }
-}
-
 extension UIViewController {
     public func auh_dismissViewController() {
         dismiss(animated: true)
